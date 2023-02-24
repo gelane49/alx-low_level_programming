536b4385d7d1f5a3fe28d8d7b52f71b4ae50cfe9
gelane49 <gelanegachena49@gmail.com>
2023-02-24 01:59:22 -0800

    0-main.h

---
+++ 0x03-debugging/main.h
@@ -0,0 +1,11 @@
+#indef MAIN_H
+
+#define MAIN_H
+
+#include <stdio.h>
+int postitive_or_negative(int i)
+int 2-largest_number.c(int a, int b, int c, );
+int convert_day(int month, int day); 
+void print_remaining_days(int month ,int day, int year);
+
+#endif /* MAIN_H */i
